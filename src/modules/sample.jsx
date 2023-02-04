39c8e32184854ba2b429b593e02cda51bcd43d43
vitamin777 <rlaalsdn8@gmail.com>
2023-02-04 23:54:47 +0900

    feat: 리덕스 미들웨어 통한 비동기 작업관리 redux-saga 를 활용해 기능 적용(counter,api)
    -1- counter 비동기 생성
    -1-01- modules/counter.jsx 에서 액션 타입 선언 및 액션 생성 함수, 제너레이터 함수 사용해 1초 딜레이 적용
    -1-02- modules/index.jsx 에서 rootSaga 제너레이터 함수 생성 후 counterSaga 등록
    -1-03- index.js 에서 스토어에 redux-saga middleware 적용
    -1-04- App.js 에서 CounterContainer 렌더링
    (counter 정상 작동 확인)
    
    -2- api 요청 상태 관리
    -2-01- modules/sample.jsx 에서 액션 타입 선언 및 액션 생성 함수, 제너레이터 함수 사용해 getPost,getUsers 요청 로직 작성
    -2-02- modules/index.jsx 에서 rootSaga 에 sampleSaga 등록
    -2-03- App.js 에서 SampleContainer 컴포넌트 렌더링
    (post,users API 응답 정상 출력 확인 및 redux devtools 로 api 비동기 요청 상태 관리 정상 확인)

--- src/modules/sample.jsx
+++ src/modules/sample.jsx
@@ -1,8 +1,11 @@
 // 액션 타입을 선언
 // 한 요청당 세 개를 만듦
 import * as api from "../lib/api";
-import { handleActions } from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
 import createRequestThunk from "../lib/createRequestThunk";
+import { finishLoading, startLoading } from "./loading";
+import { put, call } from "redux-saga/effects";
+import { takeLatest } from "redux-saga/effects";
 
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
@@ -12,10 +15,65 @@ const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
 const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
+// saga 사용 액션 생성 함수
+/* getPost 경우에 api 요청을 할때 어떤 id 로 조회할지 정해줘야하며
+ * redux-saga 를 사용할때 id 처럼 요청에 필요한 값을 액션의 payload 로 넣어줘야함
+ */
+export const getPost = createAction(GET_POST, (id) => id);
+export const getUsers = createAction(GET_USERS);
+
+function* getPostSaga(action) {
+  yield put(startLoading(GET_POST)); // 로딩 시작
+  // 파라미터로 action 을 받아오면 액션의 정보를 조회할 수 있음
+  try {
+    // call 을 사용하고 나면, 이 함수는 Promise 를 반환하고, 기다릴 수 있음
+    // 첫 번째 파라미터는 호출하고싶은 함수, 나머지 파라미터는 해당 함수에 넣어주고싶은 인수
+    const post = yield call(api.getPost, action.payload); // api.getPost(action.payload) 를 의미
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post.data,
+    });
+  } catch (e) {
+    // try/catch 문을 사용하여 에러도 잡을 수 있음
+    yield put({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_POST)); // 로딩 끝
+}
+
+function* getUsersSaga() {
+  yield put(startLoading(GET_USERS)); // 로딩 시작
+  // 파라미터로 action 을 받아오면 액션의 정보를 조회할 수 있음
+  try {
+    const users = yield call(api.getUsers); // api.getUsers(action.payload) 를 의미
+    yield put({
+      type: GET_USERS_SUCCESS,
+      payload: users.data,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_USERS_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_USERS)); // 로딩 끝
+}
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
+
 // thunk 함수를 생성
-// thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치합니다.
+// thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치함
+/* saga 를 사용하지 않고 thunk 를 사용할 경우 아래와 같이 thunk 함수를 만들어야 함
+위에 saga 사용으로 아래 thunk 함수 사용하지 않음
 export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);*/
 /* src/lib/createRequestThunk.js 파일에서 작성한 thunk 함수를 사용하여 각 액션에 대한 thunk 함수를 생성
 하였으므로 아래와 같이 각 액션에 대한 thunk 함수를 만들 필요가 없어졌음
 export const getPost = (id) => async (dispatch) => {
